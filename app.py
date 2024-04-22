d42680efdef0367e4f5ad9b9e14d8227563590b5
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2024-04-22 23:15:39 +0530

    setting up flask

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import streamlit as st
+from flask import Flask, request, redirect
+from flask_restful import Resource, Api
+# from flask_cors import CORS
 import torch
 import youtube_transcript_api
 from youtube_transcript_api import YouTubeTranscriptApi
@@ -12,79 +14,71 @@ import moviepy.editor as mp
 from youtube_transcript_api import TranscriptsDisabled
 from pydub import AudioSegment
 
-def convert_mp4_to_wav(input_file, output_file):
-    audio = AudioSegment.from_file(input_file, format="mp4")
-    audio.export(output_file, format="wav")
+app = Flask(__name__)
+# cors = CORS(app, resources={r"*": {"origins": "*"}})
+api = Api(app)
 
+class Home(Resource):
+    def get(self):
+        return 'Welcome to, Test App API!'
 
-
-# def convert_mp4_to_wav(input_file, output_file):
-#     """
-#     Converts an MP4 audio file to a WAV file.
     
-#     Args:
-#         input_file (str): Path to the input MP4 file.
-#         output_file (str): Path to the output WAV file.
-#     """
-#     # Load the MP4 file
-#     video = mp.VideoFileClip(input_file)
-    
-#     # Extract the audio from the video
-#     audio = video.audio
-    
-#     # Write the audio to a WAV file
-#     audio.write_audiofile(output_file)
-    
-#     # Close the video and audio resources
-#     video.close()
-#     audio.close()
+    def convert_mp4_to_wav(self,input_file, output_file):
+        audio = AudioSegment.from_file(input_file, format="mp4")
+        audio.export(output_file, format="wav")
 
+    def solve(self):
+        link = "https://youtu.be/RIJ2Jclv9Dg?si=SPwk1Dhw3W2EeMwe" # without subtitle
+        video_link = link
 
-
-def main():
-    st.title("YouTube Summarizer")
-
-    # Get the YouTube video link from the user
-    video_link = st.text_input("Enter the YouTube video link:")
-
-    if video_link:
-        try:
-            # Extract the unique ID from the video link
-            unique_id = video_link.split("=")[-1]
-
-            # Get the video transcript
-                    
+        if video_link:
             try:
-                sub = YouTubeTranscriptApi.get_transcript(unique_id)
-                subtitle = " ".join([x['text'] for x in sub])
-            except TranscriptsDisabled as e:
-                print("Subtitles are disabled for this video. You may need to handle this case differently.")
+                # Extract the unique ID from the video link
+                unique_id = video_link.split("=")[-1]
 
-                # Proceed with audio-based transcription
-                yt = YouTube(video_link)
-                audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
-                audio_stream.download(filename='ytaudio.mp4')
-                input_file = 'ytaudio.mp4'
-                output_file = 'ytaudio.wav'
-                convert_mp4_to_wav(input_file, output_file)
-                model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-english", device="cuda" if torch.cuda.is_available() else "cpu")
-                audio_path = ['/content/ytaudio.wav']
-                transcriptions = model.transcribe(audio_path)
-                subtitle = ' '.join([t['transcription'] for t in transcriptions])
+                try:
+                    sub = YouTubeTranscriptApi.get_transcript(unique_id)
+                    subtitle = " ".join([x['text'] for x in sub])
+                except TranscriptsDisabled as e:
+                    print("Subtitles are disabled for this video. You may need to handle this case differently.")
 
-            # Summarize the transcript using BART
-            tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-            model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
-            input_tensor = tokenizer.encode(subtitle, return_tensors="pt", max_length=512)
-            outputs_tensor = model.generate(input_tensor, max_length=160, min_length=120, length_penalty=2.0, num_beams=4, early_stopping=True)
-            summary = tokenizer.decode(outputs_tensor[0])
+                    yt = YouTube(video_link)
+                    audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
+                    audio_stream.download(filename='ytaudio.mp4')
+                    input_file = 'ytaudio.mp4'
+                    output_file = 'ytaudio.wav'
+                    self.convert_mp4_to_wav(input_file, output_file)
+                    model = SpeechRecognitionModel("jonatasgrosman/wav2vec2-large-xlsr-53-english", device="cuda" if torch.cuda.is_available() else "cpu")
+                    audio_path = ['/content/ytaudio.wav']
+                    transcriptions = model.transcribe(audio_path)
+                    subtitle = ' '.join([t['transcription'] for t in transcriptions])
 
-            # Display the summary
-            st.header("Summary:")
-            st.write(summary)
+                tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
+                model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+                input_tensor = tokenizer.encode(subtitle, return_tensors="pt", max_length=512)
+                outputs_tensor = model.generate(input_tensor, max_length=160, min_length=120, length_penalty=2.0, num_beams=4, early_stopping=True)
+                summary = tokenizer.decode(outputs_tensor[0])
 
-        except Exception as e:
-            st.error(f"Error: {e}")
+                print(summary)
+                print('done')
+            except Exception as e:
+                return (f"Error: {e}")
+
+
+    def post(self):
+        try:
+            value = request.get_json()
+            self.solve()
+            if (value):
+                return "Data received", 201
+
+            return {"error": "Invalid format."}
+
+        except Exception as error:
+            return {'error': error}
+
+api.add_resource(Home, '/')
 
 if __name__ == "__main__":
-    main()
+    port = int(5000)
+    app.run(host='0.0.0.0', port=port)
